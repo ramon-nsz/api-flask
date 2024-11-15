4eff60f3c19c6c59ecb815892cf29b7a622a37a2
slannec <nick104@outlook.com>
2024-11-15 16:45:19 -0300

    creating json routes

--- controllers/teacherController.py
+++ controllers/teacherController.py
@@ -18,6 +18,16 @@ def create_teacher():
     Teacher.add_teacher(new_teacher)
     return redirect(url_for('teachers.get_teachers'))
 
+#ROTA QUE CRIA UM PROFESSOR (RETORNA JSON)
+@teachers_blueprint.route('/teachers/create', methods=['POST'])
+def create_teacher_json():
+    new_teacher = {'nome': request.json['nome'],
+                          'idade': request.json['idade'],
+                          'materia': request.json['materia'],
+                          'observacoes': request.json['observacoes']}
+    Teacher.add_teacher(new_teacher)
+    return jsonify(Teacher.get_all())
+
 #ROTA PARA PUXAR UM PROFESSOR
 @teachers_blueprint.route('/teachers/<int:teacher_id>', methods=['GET'])
 def get_teacher(teacher_id):
@@ -26,12 +36,27 @@ def get_teacher(teacher_id):
         return render_template('teacher_id.html', teacher=teacher)
     except Teacher.TeacherNotFound:
         return jsonify({'message': 'Professor não encontrado'}), 404
+    
+#ROTA PARA PUXAR UM PROFESSOR (RETORNA JSON)
+@teachers_blueprint.route('/teacher/<int:teacher_id>', methods=['GET'])
+def get_teacher_json(teacher_id):
+    try:
+        teacher = Teacher.get_by_id(teacher_id)
+        return jsonify(teacher)
+    except Teacher.TeacherNotFound:
+        return jsonify({'message': 'Professor não encontrado'}), 404
 
 #ROTA PARA PUXAR PROFESSORES CADASTRADOS
 @teachers_blueprint.route('/teachers', methods=['GET'])
 def get_teachers():
     teachers = Teacher.get_all()
     return render_template("teachers.html", teachers=teachers)
+
+#ROTA PARA PUXAR PROFESSORES CADASTRADOS (RETORNA JSON)
+@teachers_blueprint.route('/teachers/all', methods=['GET'])
+def get_teachers_json():
+    teachers = Teacher.get_all()
+    return jsonify(teachers)
 
 #ROTA PARA ACESSAR O FORMULÁRIO DE EDIÇÃO DE UM PROFESSOR
 @teachers_blueprint.route('/teachers/<int:teacher_id>/edit', methods=['GET'])
@@ -51,6 +76,19 @@ def update_teacher(teacher_id):
         return redirect(url_for('teachers.get_teacher', teacher_id=teacher_id))
     except Teacher.TeacherNotFound:
         return jsonify({'message': 'Professor não encontrado'}), 404
+    
+#ROTA QUE EDITA O PROFESSOR (RETORNA JSON)
+@teachers_blueprint.route('/teacher/<int:teacher_id>', methods=['PUT', 'POST'])
+def update_teacher_json(teacher_id):
+    try:
+        new_data = {'nome': request.json['nome'], 
+                           'idade':request.json['idade'],
+                           'materia':request.json['materia'],
+                           'observacoes':request.json['observacoes']}
+        Teacher.update_teacher(teacher_id, new_data)
+        return jsonify(Teacher.get_by_id(teacher_id))
+    except Teacher.TeacherNotFound:
+        return jsonify({'message': 'Professor não encontrado'}), 404
 
 #ROTA PARA DELETAR UM PROFESSOR
 @teachers_blueprint.route('/teachers/<int:teacher_id>/delete', methods=['DELETE', 'POST'])
@@ -58,5 +96,14 @@ def delete_teacher(teacher_id):
     try:
         Teacher.delete_teacher(teacher_id)
         return redirect(url_for('teachers.get_teachers'))
+    except Teacher.TeacherNotFound:
+        return jsonify({'message': 'Professor não encontrado'}), 404
+    
+#ROTA PARA DELETAR UM PROFESSOR (RETORNA JSON)
+@teachers_blueprint.route('/teacher/<int:teacher_id>/delete', methods=['DELETE', 'POST'])
+def delete_teacher_json(teacher_id):
+    try:
+        Teacher.delete_teacher(teacher_id)
+        return jsonify(Teacher.get_all())
     except Teacher.TeacherNotFound:
         return jsonify({'message': 'Professor não encontrado'}), 404
